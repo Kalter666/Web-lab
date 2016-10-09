b55038dab4b265f130389e8674af5c626a6f7936
Kalter <kalterxad34@gmail.com>
2016-10-09 18:50:39 +0300

    spat'

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const template = require('consolidate');
 app.engine('pug', template.pug);
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -13,4 +14,11 @@ app.get('/lesson1', (req, res) => {
    res.render('lesson1');
 });
 
+app.get('/lesson2', (req, res) => {
+    res.render('lesson2');
+});
+app.get('/lesson3', (req, res) => {
+    res.render('lesson3');
+});
 app.listen(3000);
+console.log('server start successful at 3000');
---
+++ public/lesson3.js
@@ -0,0 +1,77 @@
+function drawBoard() {
+    $("#container").append('<table id="chessTable"></table>');
+    $('#chessTable').append('<tbody id="chessboard"></tbody>');
+    for (var i = 0; i < 10; i++) {
+        for (var j = 0; j < 10; j++) {
+            if (i == 0) {
+                $('#chessboard').append('<tr id="' + j + '"></tr>');
+                $('#' + j).append('<td id="' + (i + 1) + (j + 1) + '">' + (i + 1) + (j + 1) + '</td>');
+            } else if (j == 0) {
+                $('#' + j).append('<td id="' + (i + 1) + (j + 1) + '">' + (i + 1) + (j + 1) + '</td>');
+            } else {
+                $('#' + j).append('<td id="' + (i + 1) + (j + 1) + '">' + (i + 1) + (j + 1) + '</td>');
+            }
+        }
+    }
+}
+
+
+var lifePoints = [[], [], [], [], [], [], [], [], [], [], [], []];
+
+for (let i = 0; i < 12; i++){
+    for (let j = 0; j < 12; j++){
+        lifePoints[i][j] = false;
+    }
+}
+
+function initLife() {
+    for (var i = 1; i < 11; i++) {
+        for (var j = 1; j < 11; j++) {
+            if ((j === 4) && (i === 4) || (j === 5)) {
+                $('#' + i + j).css('background-color', '#06FF00');
+                lifePoints[i][j] = true;
+            }
+            else {
+                lifePoints[i][j] = false;
+            }
+        }
+    }
+}
+
+function life() {
+    let newLife = lifePoints;
+    let counterNeig = 0;
+    for (var i = 1; i < 11; i++) {
+        for (var j = 1; j < 11; j++) {
+            if (lifePoints[i][j] === true) {
+                if (lifePoints[i - 1][j] === true) counterNeig++;
+                if (lifePoints[i + 1][j] === true) counterNeig++;
+                if (lifePoints[i][j - 1] === true) counterNeig++;
+                if (lifePoints[i][j + 1] === true) counterNeig++;
+                if ((counterNeig < 2) || (counterNeig > 3)) newLife[i][j] = false;
+                counterNeig = 0;
+            }
+        }
+    }
+    lifePoints = newLife;
+}
+
+function reDraw() {
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            if (lifePoints[i][j] === true)
+                $('#' + (i + 1) + (j + 1)).css('background-color', '#21FF00');
+            else $('#' + (i + 1) + (j + 1)).css('background-color', 'white');
+        }
+    }
+}
+
+drawBoard();
+initLife();
+
+function zapusk() {
+    life();
+    reDraw();
+}
+
+setInterval(zapusk, 1000);
